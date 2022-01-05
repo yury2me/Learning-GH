b4b53270b571a8c75c1ab39ccbdcb2c58f6c7c4c
Iúri Tomé <iurifidalgo@gmail.com>
2022-01-05 10:45:59 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Iúri Sanchez Fidalgo Amaral Tomé on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
